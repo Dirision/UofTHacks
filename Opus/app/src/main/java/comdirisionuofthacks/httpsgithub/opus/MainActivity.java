f4860d1fd011a58e56836d2a977be6881eed3fe7
Dirision <nick.dirisio@mail.utoronto.ca>
2016-01-23 13:13:08 -0500

    fukck

--- Opus/app/src/main/java/comdirisionuofthacks/httpsgithub/opus/MainActivity.java
+++ Opus/app/src/main/java/comdirisionuofthacks/httpsgithub/opus/MainActivity.java
@@ -3,11 +3,11 @@ package comdirisionuofthacks.httpsgithub.opus;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class Splash extends Activity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        setContentView(R.layout.activity_main);
     }
 }
