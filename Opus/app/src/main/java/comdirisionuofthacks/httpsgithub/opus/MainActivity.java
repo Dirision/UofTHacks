aa60397915f52c95cf2cc0fc3eb1e5155f1f124e
Dirision <nick.dirisio@mail.utoronto.ca>
2016-01-23 15:56:07 -0500

    fdfdfddf
    dfssdfsdf

--- Opus/app/src/main/java/comdirisionuofthacks/httpsgithub/opus/MainActivity.java
+++ Opus/app/src/main/java/comdirisionuofthacks/httpsgithub/opus/MainActivity.java
@@ -2,6 +2,8 @@ package comdirisionuofthacks.httpsgithub.opus;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button yourButton = (Button) findViewById(R.id.button);
+
+        yourButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(YourCurrentActivity.this, YourNewActivity.class));
+            }
+        });
     }
 }
