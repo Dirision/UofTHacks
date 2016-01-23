e4d9c2e3a7d3ff4a071f1c4cf43ed42a82a10b5d
Dirision <nick.dirisio@mail.utoronto.ca>
2016-01-23 10:49:41 -0500

    DDDD
    SDSD

--- AntiCrastinator/anticrastinator/src/main/java/comdirisionuofthacks/httpsgithub/anticrastinator/SplashActivity.java
+++ AntiCrastinator/anticrastinator/src/main/java/comdirisionuofthacks/httpsgithub/anticrastinator/SplashActivity.java
@@ -1,12 +1,14 @@
 package comdirisionuofthacks.httpsgithub.anticrastinator;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -106,6 +108,15 @@ public class SplashActivity extends AppCompatActivity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
+        Button btn = (Button)findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, MenuActivity.class));
+            }
+        });
+
     }
 
     @Override
@@ -160,4 +171,5 @@ public class SplashActivity extends AppCompatActivity {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
+
 }
