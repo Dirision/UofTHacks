c22c310b5cdad1495ad5270750fe761178e06b39
Dirision <nick.dirisio@mail.utoronto.ca>
2016-01-23 12:33:34 -0500

    fuck this

---
+++ Opus/app/src/main/java/comdirisionuofthacks/httpsgithub/opus/Splash.java
@@ -0,0 +1,13 @@
+package comdirisionuofthacks.httpsgithub.opus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
